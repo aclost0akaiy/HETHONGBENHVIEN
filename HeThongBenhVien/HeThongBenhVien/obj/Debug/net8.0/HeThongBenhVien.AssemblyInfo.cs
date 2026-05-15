6b584e36a453afd0cf4f77044ec4d1d958e4d556
aclost0akaiy <danhg434@gmail.com>
2026-05-15 08:55:35 +0700

    up

--- HeThongBenhVien/HeThongBenhVien/obj/Debug/net8.0/HeThongBenhVien.AssemblyInfo.cs
+++ HeThongBenhVien/HeThongBenhVien/obj/Debug/net8.0/HeThongBenhVien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeThongBenhVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c94ef7ee4be414aae7d1d963b222250d4dce157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ce590e98150602aaeebe469adcbe3a73cbdc84")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeThongBenhVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeThongBenhVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
